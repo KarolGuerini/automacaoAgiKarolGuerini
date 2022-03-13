6adf13a409bd143f504d34670aae2ae4e307c18d
KarolGuerini <karoltech20@gmail.com>
2022-03-13 11:20:16 -0300

    criacao classe PesquisasTest alteracoes chromedriver e pom

---
+++ src/test/java/tests/PesquisasTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import core.Driver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PesquisasTest extends Driver {
+
+    public WebDriver navegador = creatChrome();
+
+    @Test
+    public void fazerPesquisaComValorValido(){
+        navegador.findElement(By.id("search-open")).click();
+        navegador.findElement(By.className("search-field")).sendKeys("evento");
+        navegador.findElement(By.className("search-submit")).submit();
+        navegador.findElement(By.xpath("//span[contains(.,'evento')]"));
+
+    }
+}
