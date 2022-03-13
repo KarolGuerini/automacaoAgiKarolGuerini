3f8b7d9351779221d243be9c25158451ca4659a8
KarolGuerini <karoltech20@gmail.com>
2022-03-13 11:34:10 -0300

    refatoracao e geracao de metodos

--- src/test/java/tests/PesquisasTest.java
+++ src/test/java/tests/PesquisasTest.java
@@ -10,22 +10,46 @@ public class PesquisasTest extends Driver {
     public WebDriver navegador = creatChrome();
 
     @Test
-    public void fazerPesquisaComValorValido(){
-        navegador.findElement(By.id("search-open")).click();
-        navegador.findElement(By.className("search-field")).sendKeys("evento");
-        navegador.findElement(By.className("search-submit")).submit();
-        navegador.findElement(By.xpath("//span[contains(.,'evento')]"));
-
+    public void fazerPesquisaComValorValido() {
+        clicarNoIconeDePesquisa();
+        digitarNoCampoDePesquisa("evento");
+        submeterAPesquisa();
+        resultadoDaPesquisaValida("evento");
+        fecharNavegador();
     }
+
 
     @Test
-    public void fazerPesquisaComValorInvalido(){
-        navegador.findElement(By.id("search-open")).click();
-        navegador.findElement(By.className("search-field")).sendKeys("@#$%");
-        navegador.findElement(By.className("search-submit")).submit();
-        navegador.findElement(By.xpath("//h1[contains(.,'Nenhum resultado')]"));
-
+    public void fazerPesquisaComValorInvalido() {
+        clicarNoIconeDePesquisa();
+        digitarNoCampoDePesquisa("#$%&");
+        submeterAPesquisa();
+        resultadoDaPesquisaInvalida();
+        fecharNavegador();
     }
 
+    private void clicarNoIconeDePesquisa() {
+        navegador.findElement(By.id("search-open")).click();
+    }
+
+    private void digitarNoCampoDePesquisa(String pesquisa) {
+        navegador.findElement(By.className("search-field")).sendKeys(pesquisa);
+    }
+
+    public void submeterAPesquisa() {
+        navegador.findElement(By.className("search-submit")).submit();
+    }
+
+    public void resultadoDaPesquisaValida(String pesquisa) {
+        navegador.findElement(By.xpath("//span[contains(.,'" + pesquisa + "')]"));
+    }
+
+    public void resultadoDaPesquisaInvalida() {
+        navegador.findElement(By.xpath("//h1[contains(.,'Nenhum resultado')]"));
+    }
+
+    public void fecharNavegador() {
+        navegador.quit();
+    }
 
 }
